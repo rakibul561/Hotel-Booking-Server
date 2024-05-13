446a6527d8d7959c646f8b93131d0f39088de7b9
Rakib <rakibulhasan3929@gmail.com>
2024-05-13 23:51:51 +0600

    added delete oparetion

--- index.js
+++ index.js
@@ -64,26 +64,35 @@ async function run() {
     })
 
     // bookings
-     app.get('/bookings', async (req, res)=>{
+    app.get('/bookings', async (req, res) => {
       console.log(req.query.email);
       let query = {};
-      if(req.query?.email){
-        query = {email: req.query.email}
+      if (req.query?.email) {
+        query = { email: req.query.email }
       }
-       const result = await bookingCollection.find(query).toArray();
-       res.send(result)
-     })
+      const result = await bookingCollection.find(query).toArray();
+      res.send(result)
+    })
 
 
     // this is a bookings
-    app.post('/bookings', async(req, res)=>{
-         const booking = req.body;
-         console.log(booking);
-         const result = await bookingCollection.insertOne(booking);
-         res.send(result)
+    app.post('/bookings', async (req, res) => {
+      const booking = req.body;
+      console.log(booking);
+      const result = await bookingCollection.insertOne(booking);
+      res.send(result)
     });
 
 
+    // delete oparetion
+
+    app.delete('/bookings/:id', async (req, res) => {
+      const id = req.params.id;
+      const query = { _id: new ObjectId(id) }
+      const result = await bookingCollection.deleteOne(query);
+      res.send(result);
+
+    })
 
 
 
